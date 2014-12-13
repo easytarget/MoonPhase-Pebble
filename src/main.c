40f199d4479986ca1778cf2ac8977e1b26930fd0
Owen <git@easytarget.me.uk>
2014-12-13 22:00:25 +0100

    Format, add date, smaller image, icon
    Cleaning things up, getting visual format right.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 static Window *s_window;
 static BitmapLayer *s_moon_layer;
 static GBitmap *s_moon_bitmap;
-//static TextLayer *s_date_layer;
+static TextLayer *s_date_layer;
 static TextLayer *s_time_layer;
 
 static void update_time() {
@@ -27,36 +27,53 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
+static void update_date() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "DDD\n00 MMM";
+
+  // Write the current hours and minutes into the buffer
+  strftime(buffer, sizeof("DDD\n00 MMM"), "%a\n%e %b", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_date_layer, buffer);
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  update_date();
 }
 
 static void main_window_load(Window *window) {
   // Graphics layer for the background
   s_moon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_MOON_FULL);
-  s_moon_layer = bitmap_layer_create(GRect(0, 0, 144, 144));
+  s_moon_layer = bitmap_layer_create(GRect(2, 2, 140, 140));
   bitmap_layer_set_bitmap(s_moon_layer, s_moon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_moon_layer));
   
   // Two text layers, one for static text, one for time
-	//s_date_layer = text_layer_create(GRect(25, 10, 100,100));
-	s_time_layer = text_layer_create(GRect(0, 134, 144, 34));
+	s_date_layer = text_layer_create(GRect(90,130,50,38));
+	s_time_layer = text_layer_create(GRect(6,134,90,34));
 	
 	// Set the color, text, font, and text alignment
-  //text_layer_set_text_color(s_date_layer, GColorBlack);
-  //text_layer_set_background_color(s_date_layer, GColorWhite);
-	//text_layer_set_text(s_date_layer, "Greetings\nFrom\nOwen! :-)");
-	//text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-	//text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_background_color(s_date_layer, GColorClear);
+	text_layer_set_text(s_date_layer, "DDD\n00 MMM");
+	//text_layer_set_text(s_date_layer, "Sat\n13 Dec");
+	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+	text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
   
   text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text(s_time_layer, "00:00");
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
-	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 	
 	// Add the text layers to the main window
-	//layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_date_layer));
+	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_date_layer));
 	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_time_layer));
   
 	// App Logging!
@@ -65,7 +82,7 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
 	// Destroy the text layers
-	//text_layer_destroy(s_date_layer);
+	text_layer_destroy(s_date_layer);
 	text_layer_destroy(s_time_layer);
   
   // Destroy GBitmapa+Layers
