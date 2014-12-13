c11c2bed8b6838ec8b0cd45aa61a81608da63729
Owen <git@easytarget.me.uk>
2014-12-13 22:42:10 +0100

    Layout changes, remove logging for now.

--- src/main.c
+++ src/main.c
@@ -33,10 +33,10 @@ static void update_date() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "DDD\n00 MMM";
+  static char buffer[] = "DDD.\n00 MMM";
 
   // Write the current hours and minutes into the buffer
-  strftime(buffer, sizeof("DDD\n00 MMM"), "%a\n%e %b", tick_time);
+  strftime(buffer, sizeof("DDD.\n00 MMM"), "%a.\n%e %b", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_date_layer, buffer);
@@ -50,19 +50,18 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_load(Window *window) {
   // Graphics layer for the background
   s_moon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_MOON_FULL);
-  s_moon_layer = bitmap_layer_create(GRect(2, 2, 140, 140));
+  s_moon_layer = bitmap_layer_create(GRect(2, 0, 140, 140));
   bitmap_layer_set_bitmap(s_moon_layer, s_moon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_moon_layer));
   
   // Two text layers, one for static text, one for time
-	s_date_layer = text_layer_create(GRect(90,130,50,38));
-	s_time_layer = text_layer_create(GRect(6,134,90,34));
+	s_date_layer = text_layer_create(GRect(94,130,50,37));
+	s_time_layer = text_layer_create(GRect(1,133,93,34));
 	
 	// Set the color, text, font, and text alignment
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_background_color(s_date_layer, GColorClear);
-	text_layer_set_text(s_date_layer, "DDD\n00 MMM");
-	//text_layer_set_text(s_date_layer, "Sat\n13 Dec");
+	text_layer_set_text(s_date_layer, "DDD.\n00 MMM");
 	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
   
@@ -77,7 +76,7 @@ static void main_window_load(Window *window) {
 	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_time_layer));
   
 	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Loaded the window");
+	// APP_LOG(APP_LOG_LEVEL_DEBUG, "Loaded the window");
 }
 
 static void main_window_unload(Window *window) {
@@ -90,7 +89,7 @@ static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_moon_layer);
   
 	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Unloaded the window");
+	// APP_LOG(APP_LOG_LEVEL_DEBUG, "Unloaded the window");
 }
 
 static void init(void) {
@@ -104,27 +103,29 @@ static void init(void) {
     .unload = main_window_unload
   });
   
-	// Push the window with animated=true
+  // Push the window with animated=true
 	window_stack_push(s_window, true);
 	
 	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushed the window");
+	// APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushed the window");
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-
-  // Make sure the time is displayed from the start
-  update_time();
   
+  // Make sure display is correct
+  update_time();
+  update_date();
+
 	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Registered Tick Service");
+	// APP_LOG(APP_LOG_LEVEL_DEBUG, "Registered Tick Service");
 }
 
 static void deinit(void) {
 	// Destroy the window
 	window_destroy(s_window);
+  
 	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Destroyed the window");
+	 //APP_LOG(APP_LOG_LEVEL_DEBUG, "Destroyed the window");
 }
 
 int main(void) {
